a7a261db24736d27db493397913f7801dc6ea20b
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-09-21 09:43:32 -0500

    Create index.php

---
+++ php/08_swicth/index.php
@@ -0,0 +1,49 @@
+<?php
+	/*
+		Enunciado:
+		
+		Validar Si un usuario tiene permiso a acceder a cada módulo del sistema, dependiendo su tipo de usuario
+			1.- Super Administrador
+			2.- Administrador
+			3.- Gerente
+			4.- Jefe de Marketing
+			5.- Personal de Maketing
+			6.- Jefe de Contabilidad
+			7.- Personal de Contabilidad
+			8.- Jefe de RRHH
+			9.- Jefe de Almacen
+			9.- Personal de Almacen
+		Sabiendo que los modulos exitentes en el sostema son:
+			1.- Administración
+			2.- Gerencia
+			3.- Marketing
+			4.- Contabilidad
+			5.- RRHH
+			6.- Almacen
+
+	*/
+	//Declaración de Variables (Entrada):
+?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Estructura Swtich</title>
+		<!--Stylos CSS-->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	</head>
+	<body>
+		<?php
+			//Proceso
+
+			//Fin Proceso
+
+			//Salida
+			echo "<p>".$."</p>";
+		?>
+
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	</body>
+</html>
