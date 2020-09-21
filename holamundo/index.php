398fff53c44efe52ad8dcdfaa9253eedd3217453
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-09-20 21:39:51 -0500

    update hola mundo

--- holamundo/index.php
+++ holamundo/index.php
@@ -2,3 +2,17 @@
 	//Mi Primero Hola Mundo
 	echo "Hola Mundo";
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title><?= "Hola Mundo 2"; ?></title>
+	</head>
+	<body>
+		<div>
+			<h1>
+				<?php echo "Hola Mundo"; ?>		
+			</h1>
+		</div>
+	</body>
+</html>
